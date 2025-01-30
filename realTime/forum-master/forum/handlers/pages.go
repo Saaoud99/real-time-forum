b41ee551414dea8ac8600e96bd5591a296a2ce62
Saaoud99 <saaoud.abdelhadi@gmail.com>
2025-01-30 17:31:42 +0100

    you need to fix js

--- realTime/forum-master/forum/handlers/pages.go
+++ realTime/forum-master/forum/handlers/pages.go
@@ -1,85 +1,43 @@
 package handlers
 
 import (
+	"html/template"
 	"net/http"
-
-	"forum/database"
-	"forum/utils"
 )
 
 // HomeHandler it handles requests to home page "/"
 // execute the home page and show it to the user
 func HomeHandler(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/" {
-		ErrorHandler(w, r, http.StatusNotFound)
+	tmp, err := template.ParseFiles("web/templates/index.html")
+	if err != nil {
+		// ErrorHandler(w, r, http.StatusInternalServerError)
+		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
-
-	if r.Method != http.MethodGet {
-		ErrorHandler(w, r, http.StatusMethodNotAllowed)
-		return
-	}
-
-	var IsLoggedIn bool
-
-	// check if user already logged in from another browser
-	cookie, errNoCookie := r.Cookie("session_id")
-	if errNoCookie != nil {
-		IsLoggedIn = false
-		if err := templates.ExecuteTemplate(w, "index.html", IsLoggedIn); err != nil {
-			ErrorHandler(w, r, http.StatusInternalServerError)
-			return
-		}
-	} else {
-		var isValid bool
-		if err := database.DataBase.QueryRow("SELECT EXISTS(SELECT * FROM sessions WHERE session_id=?)", cookie.Value).Scan(&isValid); err != nil {
-			ErrorHandler(w, r, http.StatusInternalServerError)
-			return
-		}
-
-		if !isValid {
-			utils.DeleteCookie(w, "session_id")
-			utils.DeleteCookie(w, "user_id")
-			utils.DeleteCookie(w, "username")
-			IsLoggedIn = false
-			if err := templates.ExecuteTemplate(w, "index.html", IsLoggedIn); err != nil {
-				ErrorHandler(w, r, http.StatusInternalServerError)
-				return
-			}
-		}
-
-		IsLoggedIn = true
-		if err := templates.ExecuteTemplate(w, "index.html", IsLoggedIn); err != nil {
-			ErrorHandler(w, r, http.StatusInternalServerError)
-			return
-		}
-	}
+	tmp.Execute(w, nil)
 }
 
-// RegisterHandler it handles requests to register page "/register"
-// parse the register page and show it to the user
-func RegisterHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodGet {
-		ErrorHandler(w, r, http.StatusMethodNotAllowed)
-		return
-	}
+// // RegisterHandler it handles requests to register page "/register"
+// // parse the register page and show it to the user
+// func RegisterHandler(w http.ResponseWriter, r *http.Request) {
+// 	if r.Method != http.MethodGet {
+// 		ErrorHandler(w, r, http.StatusMethodNotAllowed)
+// 		return
+// 	}
 
-	if err := templates.ExecuteTemplate(w, "register.html", nil); err != nil {
-		ErrorHandler(w, r, http.StatusInternalServerError)
-		return
-	}
-}
+// 	if err := templates.ExecuteTemplate(w, "register.html", nil); err != nil {
+// 		ErrorHandler(w, r, http.StatusInternalServerError)
+// 		return
+// 	}
+// }
 
-// RegisterHandler it handles requests to login page "/login"
-// parse the login page and show it to the user
-func LoginHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodGet {
-		ErrorHandler(w, r, http.StatusMethodNotAllowed)
-		return
-	}
+// // RegisterHandler it handles requests to login page "/login"
+// // parse the login page and show it to the user
+// func LoginHandler(w http.ResponseWriter, r *http.Request) {
+// 	if r.Method != http.MethodGet {
+// 		ErrorHandler(w, r, http.StatusMethodNotAllowed)
+// 		return
+// 	}
 
-	if err := templates.ExecuteTemplate(w, "login.html", nil); err != nil {
-		ErrorHandler(w, r, http.StatusInternalServerError)
-		return
-	}
-}
+	
+// }
