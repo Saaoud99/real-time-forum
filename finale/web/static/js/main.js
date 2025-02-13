02bdab2a3745eee46fc2fb9fb4b68d6dfc1d0558
Saaoud99 <saaoud.abdelhadi@gmail.com>
2025-02-13 18:12:04 +0100

    hi

--- finale/web/static/js/main.js
+++ finale/web/static/js/main.js
@@ -1,46 +1,59 @@
 import { Handlelogin } from "./login.js";
 import { Handleregister } from "./register.js";
 import { init } from "./mainn.js";
-
 const nav = (path) => {
-    window.history.pushState({}, "", path);
-    paths();
-};
+    window.history.pushState([], "", path)
+    paths()
+}
 
-function paths() {
-    if (location.pathname === "/login") {
-        Handlelogin();
-    } else if (location.pathname === "/") {
-        async function checkLogin() {
-            try {
-                const response = await fetch("/api/checklogin");
-                const data = await response.json();
+async function checkLogin() {
+    try {
+        const response = await fetch("/api/checklogin");
+        const data = await response.json();
 
-                if (data.isLoggedIn) {
-                    init();
-                } else {
-                    // Only redirect to login if the user explicitly tries to access "/"
-                    if (!window.history.state || !window.history.state.manualNavigation) {
-                        nav("/login");
-                    }
-                }
-            } catch (error) {
-                console.error("Error checking login:", error);
-            }
-        }
-
-        checkLogin();
-    } else if (location.pathname === "/register") {
-        Handleregister();
-    } else {
-        const page = document.querySelector(".zone");
-        page.innerHTML = `
-        <h1>Nothing to show</h1>
-        `;
+        console.log(data.isLoggedIn)
+        return data.isLoggedIn ;
+        // Use nullish coalescing to ensure false is returned if data.isLoggedIn is undefined
+    } catch (error) {
+        console.error("Error checking login:", error);
+        return false; // Return false in case of an error
     }
 }
 
-window.addEventListener("popstate", paths);
+async function paths() {
+    
+    console.log("chck login ===>", await checkLogin());
+    
+    if (location.pathname == "/login") {
+        
+        if (await checkLogin() === false){
+            Handlelogin();
+        }else{
+            nav("/")
+        }
+    } else if (location.pathname === "/") {
+        
+        if (await checkLogin() === false){
+            nav("/login")
+        }else{
+            init();
+        }
+              
+
+    } else if (location.pathname == "/register") {
+        if (await checkLogin()=== false){
+            Handleregister()
+        }else{
+            init();
+        }
+    }else{
+        const page = document.querySelector('.zone')
+        page.innerHTML=`
+        <h1>nothing to show<h1>
+        `
+    }
+}
 
 
 paths();
+
